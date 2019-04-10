ff5b0128bb6785cdf361d30328480b1acfc2f51a
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-10 20:48:37 +0200

    init map

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,14 +1,12 @@
 import "./../styles/appStyles.scss";
-interface ITest {
-    test: number;
-}
 const getComponent = () => {
     return import(/* webpackChunkName: "quackgame" */ "./quack-game").then((game) => {
-        const g: any = new game.QuackGame(document.getElementsByTagName("game-root")[0] as any);
+        const g: any = new game.QuackGame(document.getElementsByTagName("map-root")[0] as any);
         g.start();
     }).catch((error) => "An error occurred while loading the component");
 };
 
 getComponent().then((component) => {
-    const comp = component;
+    // tslint:disable-next-line:no-console
+    console.log("running");
 });
