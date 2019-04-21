acac5c6ec0dc433065a27e5fd9a1bb7b6e8daf78
donmahallem <donmahallem@users.noreply.github.com>
2019-04-21 15:53:17 +0200

    remove unused version

--- fetch_stops.js
+++ fetch_stops.js
@@ -1,26 +1,11 @@
 const req = require('request-promise-native');
-const git = require('simple-git');
 const fs = require('fs');
 const targetPath = __dirname + '/src/assets/stops.json';
-
-const gitAddPromise = (...files) => {
-    return new Promise((resolve, reject) => {
-        git().add(files, (err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
-    });
-}
 
 req.post('https://kvg-kiel.de/internetservice/geoserviceDispatcher/services/stopinfo/stops?top=324000000&bottom=-324000000&left=-648000000&right=648000000')
     .then((resp) => {
         fs.writeFileSync(targetPath, resp);
         return true;
-    }).then(() => {
-        return gitAddPromise(targetPath);
     }).then(() => {
         console.log("stored successful");
     }).catch((err) => {
