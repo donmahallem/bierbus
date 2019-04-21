492cdaedfb85450c75fc3185b697a9edc81407fd
donmahallem <donmahallem@users.noreply.github.com>
2019-04-21 15:50:05 +0200

    save

--- src/scripts/quack-game.ts
+++ src/scripts/quack-game.ts
@@ -8,7 +8,7 @@ export class QuackGame {
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> '
                 + 'contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
-                + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                + 'Imagery © <a href="https://www.mapbox.com/">Mapboxa</a>',
             maxZoom: 18,
             subdomains: ["a", "b", "c"],
         }).addTo(map);
